e769e8eb0f6c23f69b98e45cac024d910ff0505a
Dimitar <dimitar.ivanov.yotsov@gmail.com>
2021-01-04 22:58:42 +0100

    "first commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Container from './Container';
+import Footer from './Footer';
+import {getAllStudents} from './client';
+import AddStudentForm from "./forms/AddStudentForm";
+import {Table, Avatar, Modal, Spin, Empty} from 'antd';
+import { LoadingOutlined} from '@ant-design/icons';
+import {errorNotification} from "./Notification";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const getIndicator = () => <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+class App extends Component {
+
+    state = {
+        students: [],
+        isFetching: false,
+        isAddStudentModalVisible: false
+    }
+
+    componentDidMount() {
+        this.fetchStudents();
+    }
+
+    openAddStudentModal = () => this.setState({isAddStudentModalVisible: true});
+
+    closeAddStudentModal = () => this.setState({isAddStudentModalVisible: false});
+
+
+    fetchStudents = () => {
+        this.setState({isFetchig: true});
+        getAllStudents()
+            .then(response => response.json()
+                .then(students => {
+                    this.setState({students, isFetching: false});
+                }))
+            .catch(error => {
+                const errorMessage = error.error.message;
+                const descr = "Holly cow mate smth wrong happened...";
+                errorNotification(errorMessage, descr);
+                this.setState({isFetching: false});
+            });
+    }
+
+    render() {
+
+        const {students, isFetching, isAddStudentModalVisible} = this.state;
+
+        const commonElements = () => (
+            <div>
+                <Modal
+                    title="Add new student"
+                    visible={isAddStudentModalVisible}
+                    onOk={this.closeAddStudentModal}
+                    onCancel={this.closeAddStudentModal}
+                    width={1000}
+                >
+                    <h1>Hello from modal</h1>
+                    <AddStudentForm
+                        onSuccess={() => {
+                            this.closeAddStudentModal();
+                            this.fetchStudents();
+                        }}
+                        onFailure={(err) => {
+                            console.log(JSON.stringify(err));
+                            errorNotification("err", "zee");
+                        }}
+                    />
+                </Modal>
+                <Footer
+                    numberOfStudents={students.length}
+                    handleAddStudentClickEvent={this.openAddStudentModal}
+                />
+            </div>
+        );
+
+        if (isFetching) {
+            return (
+                <Container>
+                    <Spin indicator={getIndicator()}/>
+                </Container>
+            );
+        }
+
+        if (students && students.length) {
+
+            const columns = [
+                {
+                    title: "",
+                    key: "avatar",
+                    render: (text, student) => (
+                        <Avatar
+                            size="large"
+                        >
+                            {`${student.firstName.charAt(0).toUpperCase()}`}
+                            {`${student.lastName.charAt(0).toUpperCase()}`}
+                        </Avatar>
+                    )
+                },
+                {
+                    title: "Student Id",
+                    dataIndex: "studentId",
+                    key: "studentId"
+                },
+                {
+                    title: "First Name",
+                    dataIndex: "firstName",
+                    key: "firstName"
+                },
+                {
+                    title: "Last Name",
+                    dataIndex: "lastName",
+                    key: "lastName"
+                },
+                {
+                    title: "Email",
+                    dataIndex: "email",
+                    key: "email"
+                },
+                {
+                    title: "Gender",
+                    dataIndex: "gender",
+                    key: "gender"
+                }
+            ];
+
+            return (
+                <Container>
+                    <Table
+                        style={{marginBottom: "100px"}}
+                        dataSource={students}
+                        columns={columns}
+                        pagination={false}
+                        rowKey='studentsId'
+                    />
+                    {commonElements()}
+                </Container>
+
+            );
+        }
+        return (<Container>
+                <Empty
+                    description={
+                        <span>No students found </span>
+                    }
+                />
+                {commonElements()}
+            </Container>
+        );
+    }
 }
 
 export default App;
---
+++ src/client.js
@@ -0,0 +1,26 @@
+import fetch from 'unfetch';
+
+const checkStatus = (response) => {
+    if(response.ok) {
+        return response;
+    } else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        response.json().then(e => {
+            error.error = e;
+        });
+        return Promise.reject(error);
+    }
+}
+
+
+export const getAllStudents = () => fetch("api/students").then(checkStatus);
+
+export const addNewStudent = student =>
+    fetch("api/students", {
+        headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+        },
+        method: "POST",
+        body: JSON.stringify(student)
+    }).then(checkStatus);
