96acc7718c3c3d698caea195bbf70655633204ca
Dimitar Yotsov <63639583+dimitar-yotsov@users.noreply.github.com>
2021-01-27 19:31:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
                             this.fetchStudents();
                         }}
                         onFailure={(err) => {
-                            console.log(JSON.stringify(err));
+                            //console.log(JSON.stringify(err));
                             errorNotification("err", "zee");
                         }}
                     />
